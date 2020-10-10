9a7a9b48f66ad7a9bd725f9c516273af07f5224a
Silvio Limeira <sicemal@gmail.com>
2020-10-10 08:32:56 -0300

    fix bugs and improvements

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.demo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
 
 }
