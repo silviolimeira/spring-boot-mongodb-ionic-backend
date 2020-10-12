4708f75390f3e27ac4a17733961b53fabecc717c
Silvio Limeira <sicemal@gmail.com>
2020-10-11 21:00:42 -0300

    Create entity Post with aninhed User.

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -3,10 +3,10 @@ package com.example.demo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.domain.User;
+import com.example.demo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 
 }
