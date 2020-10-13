453b46f8213aff4dfaa40ad52dd582bbe5d565c5
Silvio Limeira <sicemal@gmail.com>
2020-10-13 11:42:00 -0300

    Query with many parameters

--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +25,24 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		//return repo.findByTitleContainingIgnoreCase(text);
+		//System.out.println("findByTitle: " + text);
 		return repo.searchTitle(text);
 	}
 	
+	//
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		//return repo.findByTitleContainingIgnoreCase(text);
+		
+		Date dt = new Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(dt); 
+		c.add(Calendar.DATE, 1);
+		dt = c.getTime();
+		maxDate = dt;
+		
+		//maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+
 }
