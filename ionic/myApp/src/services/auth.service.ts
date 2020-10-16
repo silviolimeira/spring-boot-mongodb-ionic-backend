f705e854acd2a0ca1481559d76506a2efe1ea3d4
Silvio Limeira <sicemal@gmail.com>
2020-10-16 19:27:53 -0300

    get email from token to localStorage

--- ionic/myApp/src/services/auth.service.ts
+++ ionic/myApp/src/services/auth.service.ts
@@ -5,6 +5,8 @@ import { CredentialsDTO } from 'src/models/credentials.dto';
 import { LocalUser } from 'src/models/local_user';
 import { StorageService } from './storage.service';
 
+import jwt_decode from "jwt-decode";
+
 @Injectable()
 export class AuthService {
 
@@ -19,9 +21,17 @@ export class AuthService {
     }
 
     sucessfulLogin(authorization: string) {
+
         let tok = authorization.substring(7);
+
+        let decoded = jwt_decode(tok);
+        console.log("decoded:");
+        console.log(decoded);
+
+
         let user : LocalUser = {
-            token: tok
+            token: tok,
+            email: jwt_decode(tok).sub
         }
         this.storage.setLocalUser(user);
     }
