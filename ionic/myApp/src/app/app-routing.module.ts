5236bf4ff237024637ac1d10c909b53665542644
Silvio Limeira <sicemal@gmail.com>
2020-10-16 16:45:50 -0300

    add login page

--- ionic/myApp/src/app/app-routing.module.ts
+++ ionic/myApp/src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tags',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
