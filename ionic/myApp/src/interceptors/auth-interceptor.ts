405998de02ffff246ff7483cf2dbb0ac330f1b15
silviolimeira <sicemal@gmail.com>
2020-10-17 13:33:06 -0300

    intercept header for baseUrl at API_CONFIG

--- ionic/myApp/src/interceptors/auth-interceptor.ts
+++ ionic/myApp/src/interceptors/auth-interceptor.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {Observable} from "rxjs";
 import { StorageService } from 'src/services/storage.service';
+import { API_CONFIG } from 'src/config/api.config';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -12,11 +13,17 @@ export class AuthInterceptor implements HttpInterceptor {
 
     let localUser = this.storage.getLocalUser();
 
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${localUser.token}`
-      }
-    });
+    let n = API_CONFIG.baseURL.length;
+    let requestToAPI = req.url.substring(0, n) === API_CONFIG.baseURL;
+
+    if (localUser && requestToAPI) {
+        const authReq = req.clone({
+            setHeaders: {
+              Authorization: `Bearer ${localUser.token}`
+            }
+          });
+        return next.handle(authReq);
+    }
 
     return next.handle(req);
   }
